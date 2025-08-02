40bf071d10b97f5f20ff764763c7b8fd88619a2f
debratc <136620338+debratc@users.noreply.github.com>
2025-08-02 13:06:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #
+import random
+from datetime import datetime
+
+projects = ["Project A", "Project B", "Project C", "Project D"]
+print(f"{datetime.now()}: Dự án được chọn: {random.choice(projects)}")
