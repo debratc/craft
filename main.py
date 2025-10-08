83235a1b72ef33adbfdcc3339f95f4d6fcc059a4
debratc <136620338+debratc@users.noreply.github.com>
2025-10-08 08:58:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import random
 from datetime import datetime
 
 projects = ["Project A", "Project B", "Project C", "Project D"]
-print(f"{datetime.now()}: Dự án được chọn: {random.choice(projects)}")--04--08--09--10--11--14--16--18--20--21--22--22--24--25--26--28--30--31--01--04--07--11--16--17--18--21--25--26--29--30--01--03--05
+print(f"{datetime.now()}: Dự án được chọn: {random.choice(projects)}")--04--08--09--10--11--14--16--18--20--21--22--22--24--25--26--28--30--31--01--04--07--11--16--17--18--21--25--26--29--30--01--03--05--08
